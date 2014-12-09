a8c0a3ddf8831373c537f6e4f50fb488938e0847
Mohsen Azimi <msn.azi@gmail.com>
2014-12-08 16:19:54 -0800

    Fully support converting minimal Swagger 1.2 specs file
    Implemented all required methods to support this.
    Tests are passing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var getFile = require('./get-file');
+var path = require('path');
 
 /*
  * Converts Swagger 1.2 specs file to Swagger 2.0 specs.
@@ -7,11 +8,12 @@ var getFile = require('./get-file');
  * @returns {object} - Swagger 2.0 document JSON object
 */
 module.exports = function convert(sourceUri) {
+  var basePath = path.dirname(sourceUri);
   var source = getFile(sourceUri);
   var dest = {
     swagger: '2.0',
     info: buildInfo(source),
-    paths: {}
+    paths: buildPaths(source, basePath)
   };
 
   return dest;
@@ -24,6 +26,73 @@ module.exports = function convert(sourceUri) {
 */
 function buildInfo(source) {
   var info = source.info;
+
   info.version = source.apiVersion;
+
   return info;
-};
+}
+
+/*
+ * Builds "paths" section of Swagger 2.0 document
+ * @param source {object} - Swagger 1.2 document object
+ * @param basePath {string} - base path for getting path objects
+ * @returns {object} - "paths" section of Swagger 2.0 document
+*/
+function buildPaths(source, basePath) {
+  var paths = {};
+
+  source.apis.forEach(function(api) {
+    var pathName = api.path.substr(1);
+    var oldPath = getFile(path.join(basePath, pathName + '.json'));
+
+    paths[api.path] = buildPath(oldPath);
+  });
+
+  return paths;
+}
+
+/*
+ * Builds a Swagger 2.0 path object form a Swagger 1.2 path object
+ * @param oldPath {object} - Swagger 1.2 path object
+ * @returns {object} - Swagger 2.0 path object
+*/
+function buildPath(oldPath) {
+  var path = {};
+
+  oldPath.apis.forEach(function(pathApi) {
+    pathApi.operations.forEach(function(oldOperation) {
+      path[oldOperation.method.toLowerCase()] = buildOperation(oldOperation);
+    });
+  });
+
+  return path;
+}
+
+/*
+ * Builds a Swagger 2.0 operation object form a Swagger 1.2 operation object
+ * @param oldOperation {object} - Swagger 1.2 operation object
+ * @returns {object} - Swagger 2.0 operation object
+*/
+function buildOperation(oldOperation) {
+  var operation = {responses: {}};
+
+  oldOperation.responseMessages.forEach(function(oldResponse) {
+    operation.responses[oldResponse.code] = responseMessages(oldResponse);
+  });
+
+  return operation;
+}
+
+/*
+ * Builds a Swagger 2.0 response object form a Swagger 1.2 response object
+ * @param oldResponse {object} - Swagger 1.2 response object
+ * @returns {object} - Swagger 2.0 response object
+*/
+function responseMessages(oldResponse) {
+  var response = {};
+
+  // TODO: Confirm this is correct
+  response.description = oldResponse.message;
+
+  return response;
+}
