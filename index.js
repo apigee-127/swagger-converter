235540b52a41a9b0fc73dd0b97dd752b1f4a1b40
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-07-10 16:54:12 +0300

    Switch to strict mode.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+'use strict';
 
 var assert = require('assert');
 var urlParse = require('url').parse;
